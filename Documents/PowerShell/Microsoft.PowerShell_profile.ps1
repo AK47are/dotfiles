4c53b5468b739c151d49056c858058810e0c26ea
AK47are <3318517070@qq.com>
2025-10-15 21:23:59 +0800

    feat(pwsh): add a series of alias

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -16,3 +16,16 @@ Import-Module PSCompletions
 # Completion Predictor
 Import-Module CompletionPredictor
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+
+# Customized alias https://www.xrgzs.top/posts/scoop-dev-setup
+Set-Alias -Name ping -Value Test-Connection
+Set-Alias -Name nslookup -Value Resolve-DnsName
+Set-Alias -Name ifconfig -Value Get-NetIPConfiguration
+Set-Alias -Name netstat -Value Get-NetTCPConnection
+Set-Alias -Name zip -Value Compress-Archive
+Set-Alias -Name unzip -Value Expand-Archive
+Set-Alias -Name which -Value Get-Command
+
+function .. { cd .. }
+function traceroute { Test-Connection -ComputerName $args[0] -Traceroute }
+function wget { aria2c -c -R --retry-wait=5 -x16 -s16 -j16 -k1M $args }
