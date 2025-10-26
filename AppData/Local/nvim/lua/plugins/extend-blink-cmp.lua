9719135f1fca202140e67684ea3e2402c1d34145
AK47are <3318517070@qq.com>
2025-10-26 14:07:17 +0800

    style(blink.cmp): format keybindings and cleanup

--- AppData/Local/nvim/lua/plugins/extend-blink-cmp.lua
+++ AppData/Local/nvim/lua/plugins/extend-blink-cmp.lua
@@ -4,56 +4,18 @@ return {
     keymap = {
       ["<C-l>"] = { "show", "show_documentation", "hide_documentation" },
       -- https://cmp.saghen.dev/recipes.html#select-nth-item-from-the-list
-      ["<A-1>"] = {
-        function(cmp)
-          cmp.accept({ index = 1 })
-        end,
-      },
-      ["<A-2>"] = {
-        function(cmp)
-          cmp.accept({ index = 2 })
-        end,
-      },
-      ["<A-3>"] = {
-        function(cmp)
-          cmp.accept({ index = 3 })
-        end,
-      },
-      ["<A-4>"] = {
-        function(cmp)
-          cmp.accept({ index = 4 })
-        end,
-      },
-      ["<A-5>"] = {
-        function(cmp)
-          cmp.accept({ index = 5 })
-        end,
-      },
-      ["<A-6>"] = {
-        function(cmp)
-          cmp.accept({ index = 6 })
-        end,
-      },
-      ["<A-7>"] = {
-        function(cmp)
-          cmp.accept({ index = 7 })
-        end,
-      },
-      ["<A-8>"] = {
-        function(cmp)
-          cmp.accept({ index = 8 })
-        end,
-      },
-      ["<A-9>"] = {
-        function(cmp)
-          cmp.accept({ index = 9 })
-        end,
-      },
-      ["<A-0>"] = {
-        function(cmp)
-          cmp.accept({ index = 10 })
-        end,
-      },
+      -- stylua: ignore start
+      ['<A-1>'] = { function(cmp) cmp.accept({ index = 1 }) end },
+      ['<A-2>'] = { function(cmp) cmp.accept({ index = 2 }) end },
+      ['<A-3>'] = { function(cmp) cmp.accept({ index = 3 }) end },
+      ['<A-4>'] = { function(cmp) cmp.accept({ index = 4 }) end },
+      ['<A-5>'] = { function(cmp) cmp.accept({ index = 5 }) end },
+      ['<A-6>'] = { function(cmp) cmp.accept({ index = 6 }) end },
+      ['<A-7>'] = { function(cmp) cmp.accept({ index = 7 }) end },
+      ['<A-8>'] = { function(cmp) cmp.accept({ index = 8 }) end },
+      ['<A-9>'] = { function(cmp) cmp.accept({ index = 9 }) end },
+      ['<A-0>'] = { function(cmp) cmp.accept({ index = 10 }) end },
+      -- stylua: ignore end
     },
     completion = {
       menu = {
@@ -64,7 +26,6 @@ return {
               text = function(ctx)
                 return ctx.idx == 10 and "0" or ctx.idx >= 10 and " " or tostring(ctx.idx)
               end,
-              highlight = "BlinkCmpItemIdx", -- optional, only if you want to change its color
             },
           },
         },
