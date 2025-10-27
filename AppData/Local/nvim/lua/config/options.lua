d5c1ee07e3f56528101aa654985fd51ff3705807
AK47are <3318517070@qq.com>
2025-10-27 16:28:25 +0800

    feat(nvim): add `ClearShada` command
    temporarily solve "E138: ../main.shada.tmp.X files exist, cannot
    write ShaDa file!" error

--- AppData/Local/nvim/lua/config/options.lua
+++ AppData/Local/nvim/lua/config/options.lua
@@ -29,3 +29,26 @@ vim.opt.showtabline = 0
 vim.opt.wrap = true
 -- 保证中文 wrap 正常，虽然会使得英文可读性降低
 vim.opt.linebreak = false
+
+-- https://github.com/LazyVim/LazyVim/discussions/3669
+vim.api.nvim_create_user_command("ClearShada", function()
+  local shada_path = vim.fn.expand(vim.fn.stdpath("data") .. "/shada")
+  local files = vim.fn.glob(shada_path .. "/*", false, true)
+  local all_success = 0
+  for _, file in ipairs(files) do
+    local file_name = vim.fn.fnamemodify(file, ":t")
+    if file_name == "main.shada" then
+      -- skip your main.shada file
+      goto continue
+    end
+    local success = vim.fn.delete(file)
+    all_success = all_success + success
+    if success ~= 0 then
+      vim.notify("Couldn't delete file '" .. file_name .. "'", vim.log.levels.WARN)
+    end
+    ::continue::
+  end
+  if all_success == 0 then
+    vim.print("Successfully deleted all temporary shada files")
+  end
+end, { desc = "Clears all the .tmp shada files" })
