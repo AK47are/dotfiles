161ec3d24dd7add388025b2e000d06b033b1b5e4
AK47are <3318517070@qq.com>
2025-10-31 22:04:29 +0800

    fix(nvim): bad `vim.o.shellcmdflag` setting
    persistent hang when opening a session, using the `:!<command>`, etc.
    the culprit is unknown. it happened after MSVC env was configured for me

--- AppData/Local/nvim/lua/config/options.lua
+++ AppData/Local/nvim/lua/config/options.lua
@@ -10,8 +10,7 @@ else
 end
 
 -- Setting shell command flags
-vim.o.shellcmdflag =
-  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command;"
 
 -- Setting shell redirection
 vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
