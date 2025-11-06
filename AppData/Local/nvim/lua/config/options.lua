6b8dcba68448cbefa4751809513746bedf366e62
AK47are <3318517070@qq.com>
2025-11-06 09:09:03 +0800

    feat(nvim): use LazyVim.terminal.setup("pwsh")
    - clean conflict config
    - overwrite `vim.o.shellcmdflag`, more info see
      - https://github.com/folke/persistence.nvim/issues/110
      - https://github.com/LazyVim/LazyVim/issues/4805

--- AppData/Local/nvim/lua/config/options.lua
+++ AppData/Local/nvim/lua/config/options.lua
@@ -2,25 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Check if 'pwsh' is executable and set the shell accordingly
-if vim.fn.executable("pwsh") == 1 then
-  vim.o.shell = "pwsh"
-else
-  vim.o.shell = "powershell"
-end
-
--- Setting shell command flags
-vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command;"
-
--- Setting shell redirection
-vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-
--- Setting shell pipe
-vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
-
--- Setting shell quote options
-vim.o.shellquote = ""
-vim.o.shellxquote = ""
+-- use pwsh or powershell
+require("lazyvim.util.terminal").setup("pwsh")
+vim.o.shellcmdflag =
+  "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
 
 -- hide tabline
 vim.opt.showtabline = 0
