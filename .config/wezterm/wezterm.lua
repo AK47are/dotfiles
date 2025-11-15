2bcb93be08ab478ca8894efb7cdcd4909f043980
AK47are <3318517070@qq.com>
2025-11-15 19:47:47 +0800

    fix(wezterm): optional profiles.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -21,7 +21,8 @@ else
 end
 
 -- 可以提供 profiles.lua 来添加特殊 profiles
-config.launch_menu = require("profiles") or {}
+local ok, result = pcall(require, "profiles")
+config.launch_menu = ok and result or {}
 for _, item in ipairs({
   { label = "Windows PowerShell", args = { "pwsh" } },
   { label = "命令提示符", args = { "cmd.exe" } },
