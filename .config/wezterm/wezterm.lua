7ce5a56a1646fd83f247fbbd0427f510e7b2387d
AK47are <3318517070@qq.com>
2025-11-15 09:05:55 +0800

    feat(wezterm): add profiles

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.default_prog = { "pwsh" }
-config.default_cwd = wezterm.home_dir .. "/projects"
-
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 config.hide_tab_bar_if_only_one_tab = true
 
@@ -18,9 +15,21 @@ config.font_size = 12.0
 config.line_height = 1.2
 config.cell_width = 0.8
 if wezterm.gui.get_appearance():find("Dark") then
-	config.color_scheme = "Catppuccin Frappe"
+  config.color_scheme = "Catppuccin Frappe"
 else
-	config.color_scheme = "Catppuccin Latte"
+  config.color_scheme = "Catppuccin Latte"
 end
+
+-- 可以提供 profiles.lua 来添加特殊 profiles
+config.launch_menu = require("profiles") or {}
+for _, item in ipairs({
+  { label = "Windows PowerShell", args = { "pwsh" } },
+  { label = "命令提示符", args = { "cmd.exe" } },
+}) do
+  table.insert(config.launch_menu, item)
+end
+
+config.default_prog = config.launch_menu[1].args
+config.default_cwd = wezterm.home_dir .. "/projects"
 
 return config
