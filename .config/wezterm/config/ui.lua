a4088e129e21e907a5fdd5d3ed179248257ad475
AK47are <3318517070@qq.com>
2025-11-16 15:40:27 +0800

    feat(wezterm): add colorscheme switch commands

--- .config/wezterm/config/ui.lua
+++ .config/wezterm/config/ui.lua
@@ -12,8 +12,29 @@ Config.font = Wezterm.font("0xProto Nerd Font")
 Config.font_size = 12.0
 Config.line_height = 1.2
 Config.cell_width = 0.8
+local colorscheme = {
+  dark = "Catppuccin Frappe",
+  light = "Catppuccin Latte",
+}
 if Wezterm.gui.get_appearance():find("Dark") then
-  Config.color_scheme = "Catppuccin Frappe"
+  Config.color_scheme = colorscheme.dark
 else
-  Config.color_scheme = "Catppuccin Latte"
+  Config.color_scheme = colorscheme.light
 end
+
+Wezterm.on("augment-command-palette", function(window, _)
+  return {
+    {
+      brief = "Switch to Light Colorscheme",
+      action = Wezterm.action_callback(function()
+        window:set_config_overrides({ color_scheme = colorscheme.light })
+      end),
+    },
+    {
+      brief = "Switch to Dark Colorscheme",
+      action = Wezterm.action_callback(function()
+        window:set_config_overrides({ color_scheme = colorscheme.dark })
+      end),
+    },
+  }
+end)
