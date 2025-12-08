1b4c93550cf702582e9fcf10b78dd60eef648b6c
AK47are <3318517070@qq.com>
2025-12-08 20:56:27 +0800

    feat(nvim): add keybinds for cmake-tools commands

--- AppData/Local/nvim/lua/plugins/extend-cmake-tools.lua
+++ AppData/Local/nvim/lua/plugins/extend-cmake-tools.lua
@@ -4,6 +4,9 @@ return {
     "nvim-lua/plenary.nvim",
     "folke/which-key.nvim",
   },
+  keys = {
+    { "<localleader>c", "<cmd>CMakeQuickStart<cr>", desc = "CMake: QuickStart" },
+  },
   opts = {
     cmake_build_directory = "build\\${variant:buildType}",
     cmake_runner = {
@@ -20,15 +23,23 @@ return {
     require("cmake-tools").setup(opts)
     require("which-key").add({ "<leader>m", group = "+cmake" })
     local keys = {
-      { "<leader>mb", "<cmd>CMakeBuild<cr>", desc = "CMake: Build" },
-      { "<leader>mr", "<cmd>CMakeRun<cr>", desc = "CMake: Run" },
-      { "<leader>md", "<cmd>CMakeDebug<cr>", desc = "CMake: Debug" },
-      { "<leader>mt", "<cmd>CMakeRunTest<cr>", desc = "CMake: Test" },
       { "<leader>mc", "<cmd>CMakeClean<cr>", desc = "CMake: Clean" },
+      { "<leader>ml", "<cmd>CMakeSelectLaunchTarget<cr>", desc = "CMake: Select Launch Target" },
+      { "<leader>mt", "<cmd>CMakeRunTest<cr>", desc = "CMake: Test" },
+      { "<leader>mT", "<cmd>CMakeSelectBuildType<cr>", desc = "CMake: Select Build Type" },
+
+      { "<leader>mb", "<cmd>CMakeQuickBuild<cr>", desc = "CMake: Quick Build" },
+      { "<leader>mB", "<cmd>CMakeBuild<cr>", desc = "CMake: Build" },
+      { "<leader>mr", "<cmd>CMakeQuickRun<cr>", desc = "CMake: Quick Run" },
+      { "<leader>mR", "<cmd>CMakeRun<cr>", desc = "CMake: Run" },
+      { "<leader>md", "<cmd>CMakeQuickDebug<cr>", desc = "CMake: Quick Debug" },
+      { "<leader>mD", "<cmd>CMakeDebug<cr>", desc = "CMake: Debug" },
+      { "<leader>ms", "<cmd>CMakeStopRunner<cr>", desc = "CMake: Stop Runner" },
+      { "<leader>mS", "<cmd>CMakeStopExecutor<cr>", desc = "CMake: Stop Executor" },
       { "<leader>me", "<cmd>CMakeCloseExecutor<cr>", desc = "CMake: Close Executor" },
       { "<leader>mE", "<cmd>CMakeOpenExecutor<cr>", desc = "CMake: Open Executor" },
       { "<leader>mw", "<cmd>CMakeCloseRunner<cr>", desc = "CMake: Close Runner" },
-      { "<leader>mW", "<cmd>CMakeOpenRunner<cr>", desc = "CMake: Oepn Runner" },
+      { "<leader>mW", "<cmd>CMakeOpenRunner<cr>", desc = "CMake: Open Runner" },
     }
     for _, key in ipairs(keys) do
       vim.keymap.set("n", key[1], key[2], { desc = key.desc, silent = true })
