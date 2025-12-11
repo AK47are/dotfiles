b9116f8b1cb25040a0839ff85c730613131d1019
AK47are <3318517070@qq.com>
2025-12-11 11:24:06 +0800

    feat(nvim): add codecompanion initial config

---
+++ AppData/Local/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    version = "v17.33.0",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "MeanderingProgrammer/render-markdown.nvim",
+      "HakonHarnes/img-clip.nvim",
+      "folke/snacks.nvim",
+    },
+    keys = {
+      { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "AI: Toggle Chat" },
+      { "<leader>aa", "<cmd>CodeCompanionActions<cr>", desc = "AI: Select Action" },
+      { "<leader>ai", mode = { "n", "v" }, "<cmd>CodeCompanion<cr>", desc = "AI: Inline Chat" },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = {
+            name = "deepseek",
+            model = "deepseek-chat",
+          },
+        },
+        inline = { adapter = "deepseek" },
+        cmd = { adapter = "deepseek" },
+      },
+      adapters = {
+        acp = { opts = { show_defaults = false } },
+        http = { opts = { show_defaults = false } },
+      },
+      display = { action_palette = { provider = "snacks" } },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" },
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    opts = {
+      filetypes = {
+        codecompanion = {
+          prompt_for_file_name = false,
+          template = "[Image]($FILE_PATH)",
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+}
--- AppData/Local/nvim/lua/plugins/extend-snacks.lua
+++ AppData/Local/nvim/lua/plugins/extend-snacks.lua
@@ -1,147 +1,145 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        preset = {
-          header = (function()
-            if vim.fn.winheight(0) <= 30 then
-              return ""
-            end
-            return nil
-          end)(),
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = (function()
+          if vim.fn.winheight(0) <= 30 then
+            return ""
+          end
+          return nil
+        end)(),
+      },
+    },
+    terminal = {
+      win = {
+        keys = {
+          term_normal = false,
+          nav_h = { "<C-h>", mode = "t", false },
+          nav_j = { "<C-j>", mode = "t", false },
+          nav_k = { "<C-k>", mode = "t", false },
+          nav_l = { "<C-l>", mode = "t", false },
         },
       },
-      terminal = {
-        win = {
-          keys = {
-            term_normal = false,
-            nav_h = { "<C-h>", mode = "t", false },
-            nav_j = { "<C-j>", mode = "t", false },
-            nav_k = { "<C-k>", mode = "t", false },
-            nav_l = { "<C-l>", mode = "t", false },
-          },
-        },
-      },
-      picker = {
-        sources = {
-          projects = {
-            max_depth = 3,
-            patterns = {
-              ".git",
-              ".project",
-              ".root",
+    },
+    picker = {
+      sources = {
+        projects = {
+          max_depth = 3,
+          patterns = {
+            ".git",
+            ".project",
+            ".root",
 
-              -- Java Projects
-              "pom.xml", -- Maven
-              "pom.yml", -- Maven
-              "pom.yaml", -- Maven
-              "build.gradle", -- Gradle
-              "settings.gradle", -- Gradle
-              "build.gradle.kts", -- Gradle Kotlin DSL
-              ".classpath", -- Eclipse project
+            -- Java Projects
+            "pom.xml", -- Maven
+            "pom.yml", -- Maven
+            "pom.yaml", -- Maven
+            "build.gradle", -- Gradle
+            "settings.gradle", -- Gradle
+            "build.gradle.kts", -- Gradle Kotlin DSL
+            ".classpath", -- Eclipse project
 
-              -- C/C++ Projects
-              "CMakeLists.txt", -- CMake
-              "Makefile",
-              "GNUmakefile", -- Make
-              "configure",
-              "configure.ac", -- Autotools
-              "Makefile.am", -- Automake
-              "meson.build", -- Meson
-              "meson.options", -- Meson
-              "SConstruct", -- SCons
-              "BUILD",
-              "WORKSPACE", -- Bazel
-              "conanfile.txt",
-              "conanfile.py", -- Conan
-              "vcpkg.json", -- vcpkg
+            -- C/C++ Projects
+            "CMakeLists.txt", -- CMake
+            "Makefile",
+            "GNUmakefile", -- Make
+            "configure",
+            "configure.ac", -- Autotools
+            "Makefile.am", -- Automake
+            "meson.build", -- Meson
+            "meson.options", -- Meson
+            "SConstruct", -- SCons
+            "BUILD",
+            "WORKSPACE", -- Bazel
+            "conanfile.txt",
+            "conanfile.py", -- Conan
+            "vcpkg.json", -- vcpkg
 
-              -- JavaScript/TypeScript Projects
-              "package.json",
-              "package-lock.json", -- npm
-              "yarn.lock", -- Yarn
-              "pnpm-lock.yaml", -- pnpm
-              "bun.lockb", -- Bun
-              "tsconfig.json",
-              "jsconfig.json", -- TypeScript/JavaScript
-              "angular.json", -- Angular
-              "vue.config.js", -- Vue
-              "vite.config.js", -- Vite/Vue
-              "next.config.js",
-              "next.config.mjs", -- Next.js
-              "nuxt.config.js",
-              "nuxt.config.ts", -- Nuxt
-              "svelte.config.js", -- Svelte
-              "webpack.config.js", -- Webpack
-              "rollup.config.js", -- Rollup
-              ".babelrc",
-              ".babelrc.js", -- Babel
-              ".eslintrc.js",
-              ".eslintrc.json", -- ESLint
-              ".prettierrc",
-              ".prettierrc.js", -- Prettier
+            -- JavaScript/TypeScript Projects
+            "package.json",
+            "package-lock.json", -- npm
+            "yarn.lock", -- Yarn
+            "pnpm-lock.yaml", -- pnpm
+            "bun.lockb", -- Bun
+            "tsconfig.json",
+            "jsconfig.json", -- TypeScript/JavaScript
+            "angular.json", -- Angular
+            "vue.config.js", -- Vue
+            "vite.config.js", -- Vite/Vue
+            "next.config.js",
+            "next.config.mjs", -- Next.js
+            "nuxt.config.js",
+            "nuxt.config.ts", -- Nuxt
+            "svelte.config.js", -- Svelte
+            "webpack.config.js", -- Webpack
+            "rollup.config.js", -- Rollup
+            ".babelrc",
+            ".babelrc.js", -- Babel
+            ".eslintrc.js",
+            ".eslintrc.json", -- ESLint
+            ".prettierrc",
+            ".prettierrc.js", -- Prettier
 
-              -- Python Projects
-              "pyproject.toml", -- PEP 518
-              "setup.py",
-              "setup.cfg", -- setuptools
-              "requirements.txt",
-              "Pipfile", -- pipenv
-              "environment.yml", -- conda
-              "tox.ini", -- tox
-              "pytest.ini",
-              "manage.py", -- Django
-              "scrapy.cfg", -- Scrapy
-              "wsgi.py",
-              "asgi.py", -- ASGI/WSGI
-              "conftest.py", -- pytest config
-              ".python-version", -- pyenv
+            -- Python Projects
+            "pyproject.toml", -- PEP 518
+            "setup.py",
+            "setup.cfg", -- setuptools
+            "requirements.txt",
+            "Pipfile", -- pipenv
+            "environment.yml", -- conda
+            "tox.ini", -- tox
+            "pytest.ini",
+            "manage.py", -- Django
+            "scrapy.cfg", -- Scrapy
+            "wsgi.py",
+            "asgi.py", -- ASGI/WSGI
+            "conftest.py", -- pytest config
+            ".python-version", -- pyenv
 
-              -- .NET Projects
-              "*.sln", -- Visual Studio Solution
-              "*.csproj", -- C# Project
-              "*.vbproj", -- VB.NET Project
-              "*.fsproj", -- F# Project
+            -- .NET Projects
+            "*.sln", -- Visual Studio Solution
+            "*.csproj", -- C# Project
+            "*.vbproj", -- VB.NET Project
+            "*.fsproj", -- F# Project
 
-              -- Dart & Flutter Projects
-              "pubspec.yaml",
+            -- Dart & Flutter Projects
+            "pubspec.yaml",
 
-              -- Elixir Projects
-              "mix.exs",
+            -- Elixir Projects
+            "mix.exs",
 
-              -- Haskell Projects
-              "*.cabal",
-              "stack.yaml",
+            -- Haskell Projects
+            "*.cabal",
+            "stack.yaml",
 
-              -- Godot Engine Projects
-              "project.godot",
+            -- Godot Engine Projects
+            "project.godot",
 
-              -- Nix Projects
-              "flake.nix",
-              "default.nix",
+            -- Nix Projects
+            "flake.nix",
+            "default.nix",
 
-              -- Other Languages and Tools
-              "Cargo.toml", -- Rust
-              "go.mod",
-              "go.sum", -- Go
-              "composer.json", -- PHP
-              "Gemfile",
-              "Gemfile.lock", -- Ruby
-              "Rakefile", -- Rake
-              "deps.edn", -- Clojure
-              "project.clj", -- Leiningen
-              "shadow-cljs.edn", -- Shadow CLJS
-              "build.sbt", -- Scala
-              "Package.swift", -- Swift
-              "build.zig", -- Zig
-              "*.Rproj", -- R Studio
-              ".terraform.lock.hcl", -- Terraform
-              "Tupfile",
-              "Tupfile.lua", -- Tup
-              "justfile", -- Just
-              "Taskfile.yml", -- Task
-            },
+            -- Other Languages and Tools
+            "Cargo.toml", -- Rust
+            "go.mod",
+            "go.sum", -- Go
+            "composer.json", -- PHP
+            "Gemfile",
+            "Gemfile.lock", -- Ruby
+            "Rakefile", -- Rake
+            "deps.edn", -- Clojure
+            "project.clj", -- Leiningen
+            "shadow-cljs.edn", -- Shadow CLJS
+            "build.sbt", -- Scala
+            "Package.swift", -- Swift
+            "build.zig", -- Zig
+            "*.Rproj", -- R Studio
+            ".terraform.lock.hcl", -- Terraform
+            "Tupfile",
+            "Tupfile.lua", -- Tup
+            "justfile", -- Just
+            "Taskfile.yml", -- Task
           },
         },
       },
