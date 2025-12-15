4b240e8d0f54f27e53f5cbaa1252b21e303e5baf
AK47are <3318517070@qq.com>
2025-12-15 22:17:01 +0800

    feat(codecompanion.nvim): jump across all heading levels in chat

--- AppData/Local/nvim/lua/plugins/codecompanion.lua
+++ AppData/Local/nvim/lua/plugins/codecompanion.lua
@@ -22,6 +22,78 @@ return {
             model = "deepseek-chat",
           },
           auto_scroll = false,
+          keymaps = {
+            previous_header = {
+              modes = { n = "[[" },
+              index = 14,
+              callback = function()
+                local api = vim.api
+                local bufnr = api.nvim_get_current_buf()
+                local cursor = api.nvim_win_get_cursor(0)
+                local current_row = cursor[1] - 1
+
+                local parser = vim.treesitter.get_parser(bufnr, "markdown")
+                if parser == nil then
+                  vim.notify("Couldn't find the 'markdown' treesitter parser!")
+                  return
+                end
+
+                local root_tree = parser:parse()[1]:root()
+                local query = vim.treesitter.query.parse("markdown", [[(atx_heading) @heading]])
+
+                local from_row = 0
+                local to_row = current_row
+                local found_headings = {}
+                for id, node in query:iter_captures(root_tree, bufnr, from_row, to_row) do
+                  if query.captures[id] == "heading" then
+                    local _, _, node_end, _ = node:range()
+                    if node_end < current_row then
+                      table.insert(found_headings, node)
+                    end
+                  end
+                end
+
+                if #found_headings > 0 then
+                  local target_node = found_headings[#found_headings]
+                  vim.cmd("normal! m'")
+                  local start_row, start_col, _, _ = target_node:range()
+                  api.nvim_win_set_cursor(0, { start_row + 1, start_col })
+                end
+              end,
+              description = "Previous header",
+            },
+            next_header = {
+              modes = { n = "]]" },
+              index = 13,
+              callback = function()
+                local api = vim.api
+                local bufnr = api.nvim_get_current_buf()
+                local cursor = api.nvim_win_get_cursor(0)
+                local current_row = cursor[1] - 1
+
+                local parser = vim.treesitter.get_parser(bufnr, "markdown")
+                if parser == nil then
+                  vim.notify("Couldn't find the 'markdown' treesitter parser!")
+                  return
+                end
+
+                local root_tree = parser:parse()[1]:root()
+                local query = vim.treesitter.query.parse("markdown", [[(atx_heading) @heading]])
+
+                local from_row = current_row + 1
+                local to_row = -1
+                for id, node in query:iter_captures(root_tree, bufnr, from_row, to_row) do
+                  if query.captures[id] == "heading" then
+                    vim.cmd("normal! m'")
+                    local start_row, start_col, _, _ = node:range()
+                    api.nvim_win_set_cursor(0, { start_row + 1, start_col })
+                    return
+                  end
+                end
+              end,
+              description = "Next header",
+            },
+          },
           opts = {
             -- log_level = "DEBUG",
             system_prompt = (function()
