8af86917bc5f1110e36be75950ec6d5d0966391a
AK47are <3318517070@qq.com>
2025-12-17 15:49:10 +0800

    feat(codecompanion.nvim): support aliyun, siliconflow platforms

--- AppData/Local/nvim/lua/plugins/codecompanion.lua
+++ AppData/Local/nvim/lua/plugins/codecompanion.lua
@@ -17,6 +17,10 @@ return {
       opts = {
         language = "Chinese",
       },
+      display = {
+        chat = { auto_scroll = false },
+        action_palette = { provider = "snacks" },
+      },
       interactions = {
         chat = {
           adapter = {
@@ -58,11 +62,44 @@ return {
       },
       adapters = {
         acp = { opts = { show_defaults = false } },
-        http = { opts = { show_defaults = false } },
-      },
-      display = {
-        chat = { auto_scroll = false },
-        action_palette = { provider = "snacks" },
+        http = {
+          opts = { show_defaults = false },
+          siliconflow_deepseek = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              name = "siliconflow_deepseek",
+              url = "https://api.siliconflow.cn/v1/chat/completions",
+              env = {
+                api_key = "SILICONFLOW_API_KEY",
+              },
+              schema = {
+                model = {
+                  default = "deepseek-ai/DeepSeek-V3.2",
+                  choices = {
+                    ["deepseek-ai/DeepSeek-V3.2"] = {},
+                  },
+                },
+              },
+            })
+          end,
+
+          aliyun_deepseek = function()
+            return require("codecompanion.adapters").extend("deepseek", {
+              name = "aliyun_deepseek",
+              url = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
+              env = {
+                api_key = "DASHSCOPE_API_KEY",
+              },
+              schema = {
+                model = {
+                  default = "deepseek-v3.2",
+                  choices = {
+                    ["deepseek-v3.2"] = {},
+                  },
+                },
+              },
+            })
+          end,
+        },
       },
     },
   },
