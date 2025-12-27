cd165ba9fa965bc504556f680d9c019175bf458d
AK47are <3318517070@qq.com>
2025-12-27 17:08:43 +0800

    feat(pwsh): set shell wrapper for yazi
    see https://yazi-rs.github.io/docs/quick-start#shell-wrapper

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -33,6 +33,16 @@ function config {
   }
 }
 
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
+
 Set-Alias -Name dot -Value config
 
 function leet { nvim leetcode }
