f4ae3537095d05e3dd9062e82e4e8438be20f184
AK47are <3318517070@qq.com>
2026-03-06 15:05:13 +0800

    fix(wezterm): fix window positioning on startup
    Pass `cmd` to spawn_window to preserve system default startup args

--- .config/wezterm/config/ui.lua
+++ .config/wezterm/config/ui.lua
@@ -6,19 +6,17 @@ Config.window_padding = {
   bottom = 0,
 }
 
-Wezterm.on("gui-startup", function(_)
+Wezterm.on("gui-startup", function(cmd)
   local screen = Wezterm.gui.screens().active
   local ratio = 0.7
   local width, height = screen.width * ratio, screen.height * ratio
-  local _, _, window = Wezterm.mux.spawn_window({
-    position = {
-      x = (screen.width - width) / 2,
-      y = (screen.height - height) / 2,
-      origin = "ActiveScreen",
-    },
-  })
+
+  -- 用 cmd 确保系统默认启动参数被传递
+  local _, _, window = Wezterm.mux.spawn_window(cmd or {})
+
   -- window:gui_window():maximize()
   window:gui_window():set_inner_size(width, height)
+  window:gui_window():set_position((screen.width - width) / 2, (screen.height - height) / 2)
 end)
 
 Config.hide_tab_bar_if_only_one_tab = true
