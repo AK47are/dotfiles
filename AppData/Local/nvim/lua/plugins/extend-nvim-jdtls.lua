9620e414fb2a87fa27502ac980162d67c8d6c8ed
AK47are <3318517070@qq.com>
2026-04-10 12:17:55 +0800

    feat(jdtls.nvim): auto-organize imports on save

--- AppData/Local/nvim/lua/plugins/extend-nvim-jdtls.lua
+++ AppData/Local/nvim/lua/plugins/extend-nvim-jdtls.lua
@@ -1,39 +1,48 @@
 return {
-  {
-    "mfussenegger/nvim-jdtls",
-    opts = {
-      on_attach = function(args)
-        local buf = args.buf
-        local filename = vim.fn.expand("#" .. buf .. ":t:r")
-        if filename:match("Mapper") then
-          vim.keymap.set("n", "<leader>cgm", function()
-            local name = vim.fn.expand("%:t:r")
-            local root = LazyVim.root()
-            local search_dirs = {
-              root .. "/src/main/resources/**/",
-              root .. "/src/main/java/**/mapper/**/",
-            }
-            local files = {}
-            for _, dir in ipairs(search_dirs) do
-              local found = vim.fn.globpath(dir, name .. ".xml", false, true)
-              vim.list_extend(files, found)
-            end
+  "mfussenegger/nvim-jdtls",
+  opts = {
 
-            if #files == 0 then
-              vim.notify("No matching XML file for " .. name, vim.log.levels.WARN)
-            else
-              if #files > 1 then
-                vim.notify("Multiple XML files found, opening first: " .. #files, vim.log.levels.INFO)
-              end
-              vim.cmd("edit " .. vim.fn.fnameescape(files[1]))
-            end
-          end, { buffer = buf, silent = true, desc = "Goto Mapper XML" })
-        end
+    on_attach = function(args)
+      local buf = args.buf
 
-        if LazyVim.has("which-key.nvim") then
-          require("which-key").add({ { "<leader>cg", group = "goto", buffer = buf } })
-        end
-      end,
-    },
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = buf,
+        callback = function()
+          if LazyVim.format.enabled(buf) then
+            require("jdtls").organize_imports()
+          end
+        end,
+      })
+
+      local filename = vim.fn.expand("#" .. buf .. ":t:r")
+      if filename:match("Mapper") then
+        vim.keymap.set("n", "<leader>cgm", function()
+          local name = vim.fn.expand("%:t:r")
+          local root = LazyVim.root()
+          local search_dirs = {
+            root .. "/src/main/resources/**/",
+            root .. "/src/main/java/**/mapper/**/",
+          }
+          local files = {}
+          for _, dir in ipairs(search_dirs) do
+            local found = vim.fn.globpath(dir, name .. ".xml", false, true)
+            vim.list_extend(files, found)
+          end
+
+          if #files == 0 then
+            vim.notify("No matching XML file for " .. name, vim.log.levels.WARN)
+          else
+            if #files > 1 then
+              vim.notify("Multiple XML files found, opening first: " .. #files, vim.log.levels.INFO)
+            end
+            vim.cmd("edit " .. vim.fn.fnameescape(files[1]))
+          end
+        end, { buffer = buf, silent = true, desc = "Goto Mapper XML" })
+      end
+
+      if LazyVim.has("which-key.nvim") then
+        require("which-key").add({ { "<leader>cg", group = "goto", buffer = buf } })
+      end
+    end,
   },
 }
