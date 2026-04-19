2374b3e0ea385628d711bf16472c7ab11533e49b
AK47are <3318517070@qq.com>
2026-04-19 15:19:14 +0800

    feat(pwsh): support inserting new line by Shift+Enter

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -76,6 +76,7 @@ Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 # 添加插入模式快捷键，和 Neovim 体验统一
 Set-PSReadLineKeyHandler -Chord "Ctrl+h" -Function BackwardDeleteChar -ViMode Insert
 Set-PSReadLineKeyHandler -Chord "Ctrl+w" -Function BackwardDeleteWord -ViMode Insert
+Set-PSReadLineKeyHandler -Key "Shift+Enter" -Function AddLine
 
 # ==================================
 # 自定义通用函数
