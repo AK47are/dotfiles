bf10a60feafc1243f73e6d082e1ed1316aab33eb
AK47are <3318517070@qq.com>
2026-04-27 09:45:45 +0800

    Revert "feat(nvim): disable swapfile"
    This doesn't work, ClearShada command is still needed.
    
    This reverts commit 86fe9ae72653d577c34fd60b730a02981f580cf5.

--- AppData/Local/nvim/lua/config/options.lua
+++ AppData/Local/nvim/lua/config/options.lua
@@ -67,7 +67,6 @@ vim.opt.fileformats = { "unix", "dos", "mac" }
 vim.opt.fileformat = "unix"
 
 -- vim.g.lazyvim_prettier_needs_config = true
-vim.opt.swapfile = false
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
